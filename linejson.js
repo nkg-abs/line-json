5ebc06f51ba99416ac59750622753428ea4cada6
nkg-abs <nkg@abs.in>
2023-09-06 18:50:38 +0530

    Controlled lineJSON through size.

--- linejson.js
+++ linejson.js
@@ -1,26 +1,45 @@
-const { range, map } = require('@laufire/utils/collection');
 const { rndString } = require('@laufire/utils/random');
 
-const main = (rec, col) => {
-    const start = '{';
-    const end = '}';
-    const cols = [];
-    for(let k = 0; k < col - 1; k++){
-        cols[k] = rndString();
+const col = {
+    object: {
+        shell: ['{', '}'],
+        getBody: (key) => `"${key}": "${rndString()}"`,
+        keyTypes: {
+            number: 60,
+            random: 44,
+        },
+    },
+    array: {
+        shell:  ['[', ']'],
+        getBody: () => `"${rndString()}"`,
+        keyTypes: {
+            number: 95,
+        },
+    },
+};
+
+const main = (rec, size, type, key) => {
+    const { shell, getBody, keyTypes } = col[type];
+    const cols = keyTypes[key] * size;
+    const [start, end] = shell;
+    const colName = [];
+    for(let k = 0; k < cols; k++){
+        colName[k] = key === 'number' ? k : rndString();
     }
     
     for(let i = 0; i < rec; i++){
         let body = '';
         let j = 0;
-        while(j < col - 1){
-            body += `"${cols[j]}": "${rndString()}",`;
+        while(j < cols - 1){
+            body += getBody(colName[j])+',';
             j++;
         };
-        body += `"${cols[j - 1]}": "${rndString()}"`;
+        body += getBody(colName[j]);
         console.log(start + body + end);
     };
 };
 
-main(process.argv[2] || 10, process.argv[3] || 10);
+const [,,...rest] = process.argv;
+main(...rest);
 
 module.exports = main;
